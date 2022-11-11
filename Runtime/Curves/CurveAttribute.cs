28aee35d58a0abd41a2adacec349cb5e4ad734c9
Boleslav Horak <boleslavh@gmail.com>
2022-11-11 16:23:34 +0100

    Updated curve drawer, now interactive

--- Runtime/Curves/CurveAttribute.cs
+++ Runtime/Curves/CurveAttribute.cs
@@ -1,18 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 namespace SeweralIdeas.UnityUtils.Curves
 {
     public class CurveAttribute : PropertyAttribute
     {
-        public readonly float min;
-        public readonly float max;
+        public float min;
+        public float max;
+        public float displayHeight;
 
-        public CurveAttribute(float min, float max)
+        public CurveAttribute(float min, float max, float displayHeight)
         {
             this.min = min;
             this.max = max;
+            this.displayHeight = displayHeight;
+        }
+    
+        public CurveAttribute(float min, float max) : this(min, max, EditorGUIUtility.singleLineHeight * 4f)
+        {
         }
     }
 }
--- Runtime/Curves/IRealCurve.cs
+++ Runtime/Curves/IRealCurve.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 namespace SeweralIdeas.UnityUtils.Curves
 {
-    public interface IRangedCurve
+    public interface IRealCurve
     {
         float Evaluate(float input);
         Vector2 GetValueMinMax(Vector2 range);
