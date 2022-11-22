00f0d7596288493c49aada45e5a2acb5d249a1ad
Boleslav Horak <boleslavh@gmail.com>
2022-11-22 12:36:35 +0100

    TypeLookupDatabase restriction. MultiDictionary enumerator struct initializartion fixed on some runtimes

--- Runtime/Utils/Collections/TypeLookupDatabase.cs
+++ Runtime/Utils/Collections/TypeLookupDatabase.cs
@@ -66,7 +66,7 @@ namespace SeweralIdeas.Collections
             }
         }
 
-        public ReadonlySet<TType> GetObjectsByType<TType>() where TType : class
+        public ReadonlySet<TType> GetObjectsByType<TType>() where TType : T
         {
             if (m_objectsByType.TryGetValue(typeof(TType), out IMySet set))
             {
@@ -105,7 +105,7 @@ namespace SeweralIdeas.Collections
             m_lookupDatabase = lookupDatabase;
         }
 
-        public ReadonlySet<TType> GetObjectsByType<TType>() where TType : class => m_lookupDatabase.GetObjectsByType<TType>();
+        public ReadonlySet<TType> GetObjectsByType<TType>() where TType : T => m_lookupDatabase.GetObjectsByType<TType>();
     }
 
 
