654cb3ad41a97b64ab21dbcc791f2a09cb53ac6c
Boleslav Horak <boleslavh@gmail.com>
2023-02-07 00:02:38 +0100

    Added AudioClipCollection

---
+++ Runtime/UnityUtils/AudioClipCollection.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace SeweralIdeas.UnityUtils
+{
+    [CreateAssetMenu(menuName ="SeweralIdeas/AssetCollections/AudioClipCollection")]
+    public class AudioClipCollection : AssetCollection<AudioClip>
+    {
+        [SerializeField] public Vector2 m_pitchRange = Vector2.one;
+
+        public float PickRandomPitch()
+        {
+            return Random.Range(m_pitchRange.x, m_pitchRange.y);
+        }
+
+        public void PlayOneShot(AudioSource source)
+        {
+            source.pitch = PickRandomPitch();
+            source.PlayOneShot(PickRandom());
+        }
+    }
+}
