16fc059e9eaab0cec2b0def2863242279e821e32
Boleslav Horak <boleslavh@gmail.com>
2024-06-11 02:06:47 +0200

    Adde DelayedTextField

---
+++ Runtime/UnityUtils/DelayedTextField.cs
@@ -0,0 +1,67 @@
+using System;
+using UnityEngine;
+namespace SeweralIdeas.UnityUtils
+{
+    public static partial class SeweralGUI
+    {
+        public static string DelayedTextField(Rect position, string currentValue)
+        {
+            return DelayedText(position, currentValue, GUI.TextField);
+        }
+        
+        public static string DelayedTextArea(Rect position, string currentValue)
+        {
+            return DelayedText(position, currentValue, GUI.TextArea);
+        }
+        
+        private static string DelayedText(Rect position, string currentValue, Func<Rect, string, string> guiFunc)
+        {
+            int controlID = GUIUtility.GetControlID(FocusType.Keyboard, position);
+            GUI.SetNextControlName(controlID.ToString());
+
+            // Retrieve the intermediate value from the GUI state
+            TextFieldState state = (TextFieldState)GUIUtility.GetStateObject(typeof( TextFieldState ), controlID);
+
+            if(Event.current.type == EventType.KeyDown && Event.current.keyCode is KeyCode.Escape)
+            {
+                state.HasFocus = false;
+                GUI.FocusControl(null); // Remove focus
+                Event.current.Use();
+            }
+            
+            // Initialize the state if necessary
+            if(!state.HasFocus)
+            {
+                state.Text = currentValue;
+            }
+            else if(state.Text == null)
+            {
+                state.Text = currentValue;
+            }
+
+            bool enterPressed = (Event.current.type == EventType.KeyDown && Event.current.keyCode is KeyCode.Return or KeyCode.KeypadEnter);
+            bool lostFocus = state.HasFocus && GUI.GetNameOfFocusedControl() != controlID.ToString();
+
+            state.Text = guiFunc(position, state.Text);
+
+            if(enterPressed || lostFocus)
+            {
+                currentValue = state.Text;
+                if(enterPressed)
+                {
+                    GUI.FocusControl(null); // Remove focus
+                }
+            }
+
+            state.HasFocus = (GUI.GetNameOfFocusedControl() == controlID.ToString());
+
+            return currentValue;
+        }
+
+        private class TextFieldState
+        {
+            public string Text;
+            public bool HasFocus;
+        }
+    }
+}
