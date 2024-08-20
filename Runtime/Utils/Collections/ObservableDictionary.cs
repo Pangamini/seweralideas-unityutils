f9d1ffa92df2e3470ffecdaf72d7212a51f8d2c7
Bolek <boleslav.horak@games-farm.com>
2024-08-20 14:59:44 +0200

    Added ObservableDictionary

---
+++ Runtime/Utils/Collections/ObservableDictionary.cs
@@ -0,0 +1,131 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace SeweralIdeas.Collections
+{
+    public interface IObservableDictionary : IReadonlyObservableDictionary
+    {
+    }
+
+    public interface IReadonlyObservableDictionary : IEnumerable
+    {
+        Type GetKeyType();
+        Type GetValueType();
+        
+        int Count { get; }
+    }
+
+    public interface IObservableDictionary<TKey, TVal> : IObservableDictionary, IReadonlyObservableDictionary<TKey, TVal> { }
+
+    public interface IReadonlyObservableDictionary<TKey, TVal> : IReadonlyObservableDictionary, IEnumerable<KeyValuePair<TKey, TVal>>
+    {
+        public event Action<TKey, TVal> Added;
+        public event Action<TKey, TVal> Removed;
+        public bool Contains(TKey element);
+    }
+
+    public class ObservableDictionary<TKey, TVal> : IObservableDictionary<TKey, TVal>
+    {
+        private Dictionary<TKey, TVal> m_dict = new ();
+        public event Action<TKey, TVal> Added;
+        public event Action<TKey, TVal> Removed;
+        private readonly IReadonlyObservableDictionary<TKey, TVal> m_readonly;
+
+        public ObservableDictionary()
+        {
+            m_readonly = new ReadonlyObservableDictionary<TKey, TVal>(this);
+        }
+
+        public int Count => m_dict.Count;
+
+        public Type GetKeyType() => typeof(TKey);
+        public Type GetValueType() => typeof(TVal);
+
+        public void Clear()
+        {
+            if (m_dict.Count == 0) return;
+            var set = m_dict;
+            m_dict = null;   // to prevent anyone from modifying it from the callbacks
+            if (Removed != null)
+            {
+                foreach (var obj in set)
+                    Removed(obj.Key, obj.Value);
+            }
+            set.Clear();
+            m_dict = set;
+        }
+
+        // bool ICollection<T>.IsReadOnly => false;
+
+        // void ICollection<T>.Add(T item) => Add(item);
+
+        public void Add( TKey key, TVal val )
+        {
+            m_dict.Add(key, val);
+            Added?.Invoke(key, val);
+        }
+
+        public bool Remove( TKey key )
+        {
+            var ret = m_dict.Remove(key, out var val);
+            if ( ret )
+                Removed?.Invoke(key, val);
+            return ret;
+        }
+
+        public bool Contains( TKey key ) => m_dict.ContainsKey(key);
+
+        public IReadonlyObservableDictionary<TKey, TVal> GetReadonly() => m_readonly;
+
+        public Dictionary<TKey ,TVal>.Enumerator GetEnumerator() => m_dict.GetEnumerator();
+
+        IEnumerator<KeyValuePair<TKey, TVal>> IEnumerable<KeyValuePair<TKey, TVal>>.GetEnumerator() => m_dict.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => m_dict.GetEnumerator();
+
+        public void VisitAll(Action<TKey, TVal> visitor)
+        {
+            foreach (var pair in m_dict)
+                visitor(pair.Key, pair.Value);
+        }
+    }
+
+    public class ReadonlyObservableDictionary<TKey, TVal> : IEnumerable<KeyValuePair<TKey, TVal>>, IReadonlyObservableDictionary<TKey, TVal>
+    {
+        private readonly ObservableDictionary<TKey, TVal> m_observableDict;
+
+        public ReadonlyObservableDictionary( ObservableDictionary<TKey, TVal> observableObservableSet )
+        {
+            m_observableDict = observableObservableSet ?? throw new NullReferenceException("set cannot be null");
+        }
+
+        public Type GetKeyType() => typeof(TKey);
+
+        public Type GetValueType() => typeof(TVal);
+
+        public int Count => m_observableDict.Count;
+
+        public event Action<TKey, TVal> Added
+        {
+            add => m_observableDict.Added += value;
+            remove => m_observableDict.Added -= value;
+        }
+
+        public event Action<TKey, TVal> Removed
+        {
+            add => m_observableDict.Removed += value;
+            remove => m_observableDict.Removed -= value;
+        }
+
+        public bool Contains( TKey key ) => m_observableDict.Contains(key);
+
+        IEnumerator<KeyValuePair<TKey, TVal>> IEnumerable<KeyValuePair<TKey, TVal>>.GetEnumerator() => ((IEnumerable<KeyValuePair<TKey, TVal>>)m_observableDict).GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable<KeyValuePair<TKey, TVal>>)m_observableDict).GetEnumerator();
+
+        public Dictionary<TKey, TVal>.Enumerator GetEnumerator() => m_observableDict.GetEnumerator();
+        
+        public void VisitAll(Action<TKey, TVal> visitor) => m_observableDict.VisitAll(visitor);
+    }
+}
---
+++ Runtime/Utils/Collections/ObservableSet.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace SeweralIdeas.Collections
+{
+    public static class SetExtensions
+    {
+        public static bool GetAny<T>(this IEnumerable<T> enumerable, out T value)
+        {
+            var enumerator = enumerable.GetEnumerator();
+            if (enumerator.MoveNext())
+            {
+                value = enumerator.Current;
+                return true;
+            }
+            value = default;
+            return false;
+        }
+    }
+
+    public interface IObservableSet : IReadonlyObservableSet
+    {
+    }
+
+    public interface IReadonlyObservableSet
+    {
+        Type GetContainedType();
+        
+        int Count { get; }
+    }
+
+    public interface IObservableSet<T> : IObservableSet, IReadonlyObservableSet<T> { }
+
+    public interface IReadonlyObservableSet<T> : IReadonlyObservableSet
+    {
+        public event Action<T> Added;
+        public event Action<T> Removed;
+        public bool Contains(T element);
+    }
+
+    public class ObservableSet<T> : ICollection<T>, IObservableSet<T>
+    {
+        private HashSet<T> m_set = new HashSet<T>();
+        public event Action<T> Added;
+        public event Action<T> Removed;
+        private readonly ReadonlyObservableSet<T> m_readonlyObservableSet;
+
+        public ObservableSet()
+        {
+            m_readonlyObservableSet = new ReadonlyObservableSet<T>(this);
+        }
+
+        public int Count => m_set.Count;
+
+        public Type GetContainedType() => typeof(T);
+
+        public void Clear()
+        {
+            if (m_set.Count == 0) return;
+            var set = m_set;
+            m_set = null;   // to prevent anyone from modifying it from the callbacks
+            if (Removed != null)
+            {
+                foreach (var obj in set)
+                    Removed(obj);
+            }
+            set.Clear();
+            m_set = set;
+        }
+
+        bool ICollection<T>.IsReadOnly => false;
+
+        void ICollection<T>.Add(T item) => Add(item);
+
+        public bool Add( T obj )
+        {
+            var ret = m_set.Add(obj);
+            if ( ret )
+                Added?.Invoke(obj);
+            return ret;
+        }
+
+        public bool Remove( T obj )
+        {
+            var ret = m_set.Remove(obj);
+            if ( ret )
+                Removed?.Invoke(obj);
+            return ret;
+        }
+
+        public bool Contains( T obj )
+        {
+            return m_set.Contains(obj);
+        }
+
+        public ReadonlyObservableSet<T> GetReadonly()
+        {
+            return m_readonlyObservableSet;
+        }
+
+        public HashSet<T>.Enumerator GetEnumerator()
+        {
+            return m_set.GetEnumerator();
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            return m_set.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return m_set.GetEnumerator();
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            m_set.CopyTo(array, arrayIndex);
+        }
+
+        public void VisitAll(Action<T> visitor)
+        {
+            foreach (var obj in m_set)
+            {
+                visitor(obj);
+            }
+            
+        }
+    }
+
+    public class ReadonlyObservableSet<T> : IEnumerable<T>, IReadonlyObservableSet
+    {
+        private readonly ObservableSet<T> m_observableObservableSet;
+
+        public ReadonlyObservableSet( ObservableSet<T> observableObservableSet )
+        {
+            m_observableObservableSet = observableObservableSet ?? throw new NullReferenceException("set cannot be null");
+        }
+
+        public int Count => m_observableObservableSet.Count;
+
+        public event Action<T> Added
+        {
+            add => m_observableObservableSet.Added += value;
+            remove => m_observableObservableSet.Added -= value;
+        }
+
+        public event Action<T> Removed
+        {
+            add => m_observableObservableSet.Removed += value;
+            remove => m_observableObservableSet.Removed -= value;
+        }
+
+        public bool Contains( T obj ) => m_observableObservableSet.Contains(obj);
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator() => ((IEnumerable<T>)m_observableObservableSet).GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable<T>)m_observableObservableSet).GetEnumerator();
+
+        public HashSet<T>.Enumerator GetEnumerator() => m_observableObservableSet.GetEnumerator();
+
+        Type IReadonlyObservableSet.GetContainedType() => typeof(T);
+
+        public void VisitAll(Action<T> visitor) => m_observableObservableSet.VisitAll(visitor);
+    }
+}
